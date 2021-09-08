6b90ca5941bf0a7dc854caa1a95b6ca03c4ff222
Jake Standley <jakestandley95@gmail.com>
2021-09-08 11:47:01 -0500

    added class level comment

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Hangman
+  # represents human player "playing" the game
   class Player
     # potential attributes: name
     # potential methods: guess letter
