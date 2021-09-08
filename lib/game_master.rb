c487a2708fc6df9e564b76a3571ba421fa37ae39
Jake Standley <jakestandley95@gmail.com>
2021-09-08 13:36:17 -0500

    Moved game logic into GameMaster class (from main.rb), split logic into smaller methods, added loops for player turn (entering letter) and game turn (player guesses, computer provides feedback, checks for win/loss). Also made it so player can only enter a single letter.

--- lib/game_master.rb
+++ lib/game_master.rb
@@ -4,13 +4,77 @@ module Hangman
   # represents entity managing the game
   class GameMaster
     # potential attributes: computer object, player object
+    def initialize(player, computer)
+      @player = player
+      @computer = computer
+      @dictionary_filename = '5desk.txt'
+    end
+
+    def play_game
+      load_dictionary(@dictionary_filename)
+      computer_initial_turn
+      turn_loop
+    end
 
     attr_reader :dictionary
+
+    private
 
     # potential methods: play game, declare win/loss, new game?, loop rounds, keep score, load dictionary
     def load_dictionary(file)
       # read file in then clean out words that are less than 5 or more than 12 characters
       @dictionary = File.readlines(file).select { |word| word.length >= 5 && word.length <= 12 }
+    end
+
+    def computer_initial_turn
+      @computer.select_word(dictionary)
+      selected_word = @computer.selected_word
+      puts selected_word
+
+      @computer.create_word_board
+      word_board = @computer.word_board
+      puts word_board
+    end
+
+    def player_turn
+      loop do
+        puts 'Enter a letter:'
+        @player.guess_letter
+        if @computer.correct_guesses.include?(@player.guessed_letter) || @computer.incorrect_guesses.include?(@player.guessed_letter)
+          puts 'Letter has already been guessed.'
+          next
+        elsif @player.guessed_letter.length != 1 || @player.guessed_letter =~ /\W/ || @player.guessed_letter =~ /\d/
+          puts 'Please enter one letter.'
+          next
+        else
+          break
+        end
+      end
+    end
+
+    def run_a_turn
+      player_turn
+
+      @computer.update_word_board(@player.guessed_letter)
+      word_board = @computer.word_board
+      puts word_board
+
+      puts "Correct guesses: #{@computer.correct_guesses}"
+      puts "Incorrect guesses: #{@computer.incorrect_guesses}"
+      puts "Remaining incorrect chances: #{@computer.incorrect_chances}"
+    end
+
+    def turn_loop
+      loop do
+        run_a_turn
+        if @computer.incorrect_chances.zero?
+          puts "Unfortunately you've run out of guesses and the man has been hanged. Big rip"
+          break
+        elsif !(@computer.word_board.include? '_')
+          puts "You've managed to guess the word and the man lives!"
+          break
+        end
+      end
     end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -5,56 +5,9 @@ require_relative 'computer'
 require_relative 'player'
 
 # script that will be used to run the game
-dictionary_filename = '5desk.txt'
 
-game_test = Hangman::GameMaster.new
-computer_test = Hangman::Computer.new
 player_test = Hangman::Player.new
+computer_test = Hangman::Computer.new
+game_test = Hangman::GameMaster.new(player_test, computer_test)
 
-game_test.load_dictionary(dictionary_filename)
-
-dictionary = game_test.dictionary
-
-computer_test.select_word(dictionary)
-selected_word = computer_test.selected_word
-puts selected_word
-
-computer_test.create_word_board
-word_board = computer_test.word_board
-puts word_board
-
-puts 'Enter your letter:'
-player_test.guess_letter
-guessed_letter = player_test.guessed_letter
-
-computer_test.update_word_board(guessed_letter)
-word_board = computer_test.word_board
-puts word_board
-
-puts "Correct guesses: #{computer_test.correct_guesses}"
-puts "Incorrect guesses: #{computer_test.incorrect_guesses}"
-puts "Remaining incorrect chances: #{computer_test.incorrect_chances}"
-
-puts 'Enter your letter:'
-player_test.guess_letter
-guessed_letter = player_test.guessed_letter
-
-computer_test.update_word_board(guessed_letter)
-word_board = computer_test.word_board
-puts word_board
-
-puts "Correct guesses: #{computer_test.correct_guesses}"
-puts "Incorrect guesses: #{computer_test.incorrect_guesses}"
-puts "Remaining incorrect chances: #{computer_test.incorrect_chances}"
-
-puts 'Enter your letter:'
-player_test.guess_letter
-guessed_letter = player_test.guessed_letter
-
-computer_test.update_word_board(guessed_letter)
-word_board = computer_test.word_board
-puts word_board
-
-puts "Correct guesses: #{computer_test.correct_guesses}"
-puts "Incorrect guesses: #{computer_test.incorrect_guesses}"
-puts "Remaining incorrect chances: #{computer_test.incorrect_chances}"
+game_test.play_game
