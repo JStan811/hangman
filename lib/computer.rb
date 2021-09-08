1da81bcccbdb5c9222762f626e6ef006707ff9bc
Jake Standley <jakestandley95@gmail.com>
2021-09-08 11:46:35 -0500

    Added methods to select a word and to create the word board

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,8 +2,19 @@
 
 module Hangman
   # The computer that will be picking the word to guess, managing the hangman, and providing feedback to the player
-  class Computer
+  class Computer 
     # potential attributes: selected_word
+
+    attr_reader :selected_word, :word_board
+
     # potential methods: read dictionary, select word, give feedback, update hangman (update guesses remaining), update correct list, update incorrect list
+    def select_word(dictionary)
+      @selected_word = dictionary.sample
+    end
+
+    def create_word_board
+      # need to subtract 2 from word length b/c each word ends with "/r/n"
+      @word_board = '_ ' * (@selected_word.length - 2)
+    end
   end
 end
