aae4171e2a5a626b240712bcca957efdb050725c
Jake Standley <jakestandley95@gmail.com>
2021-09-08 11:46:01 -0500

    Added method to load and clean dictionary file

--- lib/game_master.rb
+++ lib/game_master.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 module Hangman
+  # represents entity managing the game
   class GameMaster
     # potential attributes: computer object, player object
-    # potential methods: play game, declare win/loss, new game?, loop rounds, keep score
+
+    attr_reader :dictionary
+
+    # potential methods: play game, declare win/loss, new game?, loop rounds, keep score, load dictionary
+    def load_dictionary(file)
+      # read file in then trim out words that are less than 5 or more than 12 characters
+      @dictionary = File.readlines(file).select { |word| word.length >= 5 && word.length <= 12 }
+    end
   end
 end
